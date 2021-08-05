1caded98b9c01d312bdaf3797193595bc59bc2fe
picadoor1 <87449297+picadoor1@users.noreply.github.com>
2021-08-05 08:48:11 +0000

    Update oto.py

--- oto.py
+++ oto.py
@@ -10,9 +10,12 @@ print("""
 ____________________
 1- Nmap İle Hafif Port Tara
 2- Nmap İle Servis Versiyon Bilgisini Al
-3- Sqlmap İle Dbs :ek
+3- Sqlmap İle Dbs Çek
 4- Waff Bypass Sql
 5- Site İp Adresi Öğrenme
+6- WpsScan Brute Force
+7- WpsScan İle Sitede Genel Tarama Yapma
+8- MD5 Cracker
 _______________________
 """)
 vur = input("Lütfen yapacağınızı seçiniz -> ")
@@ -24,13 +27,28 @@ if vur == "2" :
    os.system("sudo nmap -sS -sV " + vur3)
 if vur == "3":
    vur4 = input("açıklı url => ")
+   os.system("service tor start")
    os.system("sqlmap "+vur4 + "--dbs --batch")
 if vur == "4":
    vur4 = input("Example : https://otuzbir.com/ => ")
-   os.system("sqlmap "+vur4 + "--forms --risk=3 --level=5 --skip-waf -v 2 --dbs --batch")
+   os.system("service tor start")
+   os.system("sqlmap "+vur4 + "--forms --risk=3 --level=5 --skip-waf -v 2 --dbs --batch --tor")
 if vur == "5":
    vur4 = input("ip'sini bulmak istediğiniz site => ")
    os.system("ping "+vur4 +"")
-else : 
+if vur == "6":
+   vur4 = input("BruteForce Yapmak İstediğiniz Wordpress Sitesi => ")
+   os.system("wpscan --url "+vur4 +" --passwords wordlist.txt --usernames user.txt")
+if vur == "7":
+   vur4 = input("Tarama Yapmak İstediğiniz Site => ")
+   os.system("wpscan --url "+vur4+" ")
+if vur == "8":
+   vur4 = input("Kırmak İstediğinz MD5 => ")
+   os.system("pip install urllib3")
+   os.system("pip install requests")
+   os.system("pip install bs4")
+   os.system("python3 md5-cracker.py "+vur4+" ")
+else   : 
   print("yanlış seçim")
+  os.system("python3 oto.py")
 
